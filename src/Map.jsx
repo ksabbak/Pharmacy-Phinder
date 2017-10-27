0543d421dcfe229f4602515d3cb23d1b8e06b180
K. Sabbak <ksabbak@gmail.com>
2017-10-26 19:15:36 -0500

    Initial commit

---
+++ src/Map.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+	render() {
+		return (
+			<div className="App">
+				<p>This is the Map. It's the map, it's the map, it's the map, it's the map it's the map, it's the map!</p>
+			</div>
+		);
+	}
+}
+
+export default Map;
