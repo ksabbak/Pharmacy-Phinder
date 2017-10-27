2634bdbb1d447f9abda7d0a684b44dc2155a48ed
K. Sabbak <ksabbak@gmail.com>
2017-10-27 01:38:05 -0500

    Begin form creation
    Create FormTextInput components, technically they also include the form label. First and Last name along with a submit button have been added. Submit currently does nothing but prevent the default.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import FormTextInput from './FormTextInput';
 
 class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: '',
+			lastName: ''
+		};
+
+		this.handleInput = this.handleInput.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleInput(updatedState) {
+		console.log(updatedState);
+		this.setState(updatedState);
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+	}
+
 	render() {
 		return (
 			<div className="Form">
-				<p>Okay, ladies, now let's get in FORMation</p>
+				<p>
+					Okay, ladies, now let's get in FORMation {this.state.firstName} {this.state.lastName}
+				</p>
+				<form>
+					<FormTextInput name="firstName" humanReadable="First Name" inputHandler={this.handleInput} />
+					<FormTextInput name="lastName" humanReadable="Last Name" inputHandler={this.handleInput} />
+					<input type="submit" name="Submit" onClick={this.handleSubmit} />
+				</form>
 			</div>
 		);
 	}
